ca9a86a7eab4c6aedd9bd45c0723e65a78cec559
Srividhya2900 <61788295+Srividhya2900@users.noreply.github.com>
2021-02-07 20:21:13 +0530

    Update week3_q3.java
    A class Shape is defined with two overloading constructors in it. Another class Test1 is partially defined which inherits the class Shape. The class Test1 should include two overloading constructors as appropriate for some object instantiation shown in main() method. You should define the constructors using the super class constructors. Also, override the method calculate( ) in Test1 to calculate the volume of a Shape.

--- week3_q3.java
+++ week3_q3.java
@@ -32,3 +32,19 @@ this.breadth= b;
 double calculate(){
   return length*breadth*height;
 }
+public static void main(String args[]){
+	Scanner sc = new Scanner(System.in);//Create an object to read                                                               
+                                          //input
+	double l=sc.nextDouble(); //Read length
+	double b=sc.nextDouble(); //Read breadth	
+	double h=sc.nextDouble(); //Read height
+	Test1 myshape1 = new Test1(l,h);
+	Test1 myshape2 = new Test1(l,b,h);
+	double volume1;
+	double volume2;
+	volume1 = myshape1.calculate();
+	volume2=myshape2.calculate();
+	System.out.println(volume1);
+	System.out.println(volume2);
+	}
+}
